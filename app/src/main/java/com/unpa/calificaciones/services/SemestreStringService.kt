c472530264a59dbb221bf7d04988eef1d2f3b06b
Adrian RB <rubioalvaradoadrian@gmail.com>
2025-05-22 11:37:43 -0600

    cambios visuales para testing

--- app/src/main/java/com/unpa/calificaciones/services/SemestreStringService.kt
+++ app/src/main/java/com/unpa/calificaciones/services/SemestreStringService.kt
@@ -1,5 +1,5 @@
 package com.unpa.calificaciones.services
 
 object SemestreStringService {
-    var semestres = arrayOf("primero", "segundo","tercero","cuarto","quinto","sexto","séptimo","octavo","noveno","decimo")
+    var semestres = arrayOf("1er Semestre", "2do Semestre","3er Semestre","4to Semestre","5to Semestre","6to Semestre","7mo Semestre","8vo Semestre","9no Semestre","10mo Semestre")
 }
